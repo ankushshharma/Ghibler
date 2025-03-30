81e172fdbd8b91d42470d20d5459095073c21525
Ankush Sharma <65322655+ankushshharma@users.noreply.github.com>
2025-03-30 16:42:03 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const PICOGEN_API_TOKEN = '8180809087d3e1a10c9f4691e653e81eefe38bfac809d79d05629b20c8189bb0'; // Replace with your actual Picogen API token
+export const PICOGEN_API_TOKEN = '818080b8f5dcaaa20ce50083739ceeb864c06e1e89b57e3228c1cd04fad182e0'; // Replace with your actual Picogen API token
